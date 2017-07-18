84e42b28fd49150df24632a7e4a1cf43a03dc848
Hyun Kim <hkim@live.com.au>
2017-07-18 16:33:53 +1000

    last commit reverted

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -42,7 +42,8 @@ class RepoList extends Component {
                 <Item.Meta>
                   <span>{repo.created_at}</span>
                 </Item.Meta>
-                <Item.Description>{repo.description.replace(/portfolio/, '')}</Item.Description>
+                {/* .replace(/portfolio/, '') */}
+                <Item.Description>{repo.description}</Item.Description>
                 <Item.Extra>
                   <Label as='a' color={skills_obj(repo.language).color} image>
                     {repo.language == null ? (null) : <img src={skills_obj(repo.language).image} />}
